ece6f11cfd42a70a25931d5a96c627836594eb8a
VENKATESH <VENKATESH@DESKTOP-R6HKVKT.lan>
2024-11-04 11:33:37 +0530

    third commit

---
+++ venky/src/basics/ifelseisladder.java
@@ -0,0 +1,32 @@
+package basics;
+
+public class ifelseisladder {
+
+	public static void main(String[] args) {
+	
+       int score=89;
+		
+		if(score>90)
+		{
+			System.out.println("grade A");
+		}
+		else if(score>=80)
+		{
+			System.out.println("grade B");
+		}
+		else if(score>=70)
+		{
+			System.out.println("grade c");
+		}
+		else if(score>60)
+		{
+			System.out.println("grade D");
+		}
+		else
+		{
+			System.out.println("fail");
+		}
+
+	}
+
+}
