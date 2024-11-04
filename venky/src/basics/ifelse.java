1f7021c2244f7d9bfd052e2fa5210015d242568f
VENKATESH <VENKATESH@DESKTOP-R6HKVKT.lan>
2024-11-04 11:01:38 +0530

    third commit

---
+++ venky/src/basics/ifelse.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		int age=11;
+		if(age>18)
+		{
+			System.out.println("you are eligible");
+		}
+		else
+		{
+			System.out.println("you are not eligible");
+		}
+
+	}
+
+}
