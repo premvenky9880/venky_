a47977b3807088673013be1e3c6d2da746abede0
VENKATESH <VENKATESH@DESKTOP-R6HKVKT.lan>
2024-11-04 13:59:08 +0530

    fifth commit

---
+++ venky/src/basics/whileloop.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class whileloop {
+
+	public static void main(String[] args) {
+		
+	
+		int i=1;
+		while(i<=10) //i<10(lesser thn 10)
+		{
+			System.out.println("Tomorrow is holoday"   +   i);
+			i++;
+		
+		
+		}
+	}
+
+}
